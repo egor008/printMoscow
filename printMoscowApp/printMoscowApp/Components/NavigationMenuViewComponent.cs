3f6922358d84640a4836daf7688b188a382619de
Egor Rozhkov <rozhkov.egor@gmail.com>
2019-05-29 23:41:29 +0300

    Добавил отображени по категориям

---
+++ printMoscowApp/printMoscowApp/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using PrintMoscowApp.Models;
+
+namespace PrintMoscowApp.Components
+{
+	public class NavigationMenuViewComponent : ViewComponent
+	{
+		private IProductRepository repository;
+
+		public NavigationMenuViewComponent(IProductRepository repo)
+		{
+			repository = repo;
+		}
+
+		public IViewComponentResult Invoke()
+		{
+			ViewBag.SelectedCategory = RouteData?.Values["category"];
+			return View(repository.Products
+				.Select(x => x.Category)
+				.Distinct()
+				.OrderBy(x => x));
+		}
+	}
+}
--- printMoscowApp/printMoscowApp/Controllers/ProductController.cs
+++ printMoscowApp/printMoscowApp/Controllers/ProductController.cs
@@ -10,27 +10,35 @@ using PrintMoscowApp.Models.ViewModels;
 
 namespace PrintMoscowApp.Controllers
 {
+
 	public class ProductController : Controller
 	{
 		private IProductRepository repository;
 		public int PageSize = 4;
+
 		public ProductController(IProductRepository repo)
 		{
 			repository = repo;
 		}
-		public ViewResult List(int page = 1)
+
+		public ViewResult List(string category, int page = 1)
 			=> View(new ProductsListViewModel
 			{
 				Products = repository.Products
-				.OrderBy(p => p.ProductID)
-				.Skip((page - 1) * PageSize)
-				.Take(PageSize),
+					.Where(p => category == null || p.Category == category)
+					.OrderBy(p => p.ProductID)
+					.Skip((page - 1) * PageSize)
+					.Take(PageSize),
 				PagingInfo = new PagingInfo
 				{
 					CurrentPage = page,
 					ItemsPerPage = PageSize,
-					TotalItems = repository.Products.Count()
-				}
+					TotalItems = category == null ?
+						repository.Products.Count() :
+						repository.Products.Where(e =>
+							e.Category == category).Count()
+				},
+				CurrentCategory = category
 			});
 	}
 }
