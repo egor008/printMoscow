baf4c2c0885eec3e4395400c539f3798256d09f1
Egor Rozhkov <rozhkov.egor@gmail.com>
2019-05-31 00:26:05 +0300

    Добавил модель заказа

---
+++ printMoscowApp/printMoscowApp/Models/Order.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using static PrintMoscowApp.Models.Cart;
+
+namespace PrintMoscowApp.Models
+{
+	public class Order
+	{
+		[BindNever]
+		public int OrderID { get; set; }
+		[BindNever]
+		public ICollection<CartLine> Lines { get; set; }
+
+		[Required(ErrorMessage = "Введите имя")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Введите первую строку адреса")]
+		public string Line1 { get; set; }
+		public string Line2 { get; set; }
+		public string Line3 { get; set; }
+		[Required(ErrorMessage = "Введите название города")]
+		public string City { get; set; }
+		[Required(ErrorMessage = "Введите название штата")]
+		public string State { get; set; }
+		public string Zip { get; set; }
+		[Required(ErrorMessage = "Введите название страны")]
+		public string Country { get; set; }
+		public string GiftWrap { get; set; }
+	}
+}
