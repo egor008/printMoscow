87a5ad57b2fe91e3f1b3ddd579c1b83215adfbb6
Egor Rozhkov <rozhkov.egor@gmail.com>
2019-05-31 00:30:57 +0300

    Страница оформления заказа

---
+++ printMoscowApp/printMoscowApp/Controllers/OrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrintMoscowApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrintMoscowApp.Controllers
+{
+	public class OrderController : Controller
+	{
+					public ViewResult Checkout() => View(new Order());
+		}
+}
