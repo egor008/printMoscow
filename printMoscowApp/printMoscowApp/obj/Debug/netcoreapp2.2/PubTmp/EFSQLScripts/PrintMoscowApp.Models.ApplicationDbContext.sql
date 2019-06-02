fc67acf6870ee2283a4258b7482ea853afa695b0
Egor Rozhkov <rozhkov.egor@gmail.com>
2019-06-02 22:16:43 +0300

    ОНО ОПУБЛИКОВАЛОСЬ

---
+++ printMoscowApp/printMoscowApp/obj/Debug/netcoreapp2.2/PubTmp/EFSQLScripts/PrintMoscowApp.Models.ApplicationDbContext.sql
@@ -0,0 +1,180 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190528200256_Initial')
+BEGIN
+    CREATE TABLE [Products] (
+        [ProductID] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        [Discription] nvarchar(max) NULL,
+        [Price] decimal(18,2) NOT NULL,
+        [Category] nvarchar(max) NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([ProductID])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190528200256_Initial')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20190528200256_Initial', N'2.2.4-servicing-10062');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190530213326_Orders')
+BEGIN
+    CREATE TABLE [Orders] (
+        [OrderID] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        [Line1] nvarchar(max) NOT NULL,
+        [Line2] nvarchar(max) NULL,
+        [Line3] nvarchar(max) NULL,
+        [City] nvarchar(max) NOT NULL,
+        [State] nvarchar(max) NOT NULL,
+        [Zip] nvarchar(max) NULL,
+        [Country] nvarchar(max) NOT NULL,
+        [GiftWrap] nvarchar(max) NULL,
+        CONSTRAINT [PK_Orders] PRIMARY KEY ([OrderID])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190530213326_Orders')
+BEGIN
+    CREATE TABLE [CartLine] (
+        [CartLineID] int NOT NULL IDENTITY,
+        [ProductID] int NULL,
+        [Quantity] int NOT NULL,
+        [OrderID] int NULL,
+        CONSTRAINT [PK_CartLine] PRIMARY KEY ([CartLineID]),
+        CONSTRAINT [FK_CartLine_Orders_OrderID] FOREIGN KEY ([OrderID]) REFERENCES [Orders] ([OrderID]) ON DELETE NO ACTION,
+        CONSTRAINT [FK_CartLine_Products_ProductID] FOREIGN KEY ([ProductID]) REFERENCES [Products] ([ProductID]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190530213326_Orders')
+BEGIN
+    CREATE INDEX [IX_CartLine_OrderID] ON [CartLine] ([OrderID]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190530213326_Orders')
+BEGIN
+    CREATE INDEX [IX_CartLine_ProductID] ON [CartLine] ([ProductID]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190530213326_Orders')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20190530213326_Orders', N'2.2.4-servicing-10062');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190601191543_ShippedOrders')
+BEGIN
+    ALTER TABLE [Orders] ADD [Shipped] bit NOT NULL DEFAULT 0;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190601191543_ShippedOrders')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20190601191543_ShippedOrders', N'2.2.4-servicing-10062');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190601195221_RenameDescription')
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Products]') AND [c].[name] = N'Discription');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Products] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Products] DROP COLUMN [Discription];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190601195221_RenameDescription')
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Products]') AND [c].[name] = N'Name');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Products] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [Products] ALTER COLUMN [Name] nvarchar(max) NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190601195221_RenameDescription')
+BEGIN
+    DECLARE @var2 sysname;
+    SELECT @var2 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Products]') AND [c].[name] = N'Category');
+    IF @var2 IS NOT NULL EXEC(N'ALTER TABLE [Products] DROP CONSTRAINT [' + @var2 + '];');
+    ALTER TABLE [Products] ALTER COLUMN [Category] nvarchar(max) NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190601195221_RenameDescription')
+BEGIN
+    ALTER TABLE [Products] ADD [Description] nvarchar(max) NOT NULL DEFAULT N'';
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190601195221_RenameDescription')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20190601195221_RenameDescription', N'2.2.4-servicing-10062');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190602000357_PrintMoscow')
+BEGIN
+    ALTER TABLE [Products] ADD [ImageData] varbinary(max) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190602000357_PrintMoscow')
+BEGIN
+    ALTER TABLE [Products] ADD [ImageMimeType] varchar(50) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20190602000357_PrintMoscow')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20190602000357_PrintMoscow', N'2.2.4-servicing-10062');
+END;
+
+GO
+
