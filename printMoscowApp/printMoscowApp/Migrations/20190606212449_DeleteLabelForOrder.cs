8616c7d39bd189c5dbe6c1172fab5ab1a051f686
Egor Rozhkov <rozhkov.egor@gmail.com>
2019-06-07 00:27:00 +0300

    не большие изменения

---
+++ printMoscowApp/printMoscowApp/Migrations/20190606212449_DeleteLabelForOrder.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrintMoscowApp.Migrations
+{
+    public partial class DeleteLabelForOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Line2",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Line3",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Orders");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Orders",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Line2",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Line3",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Orders",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
--- printMoscowApp/printMoscowApp/Models/Order.cs
+++ printMoscowApp/printMoscowApp/Models/Order.cs
@@ -20,17 +20,11 @@ namespace PrintMoscowApp.Models
 		public bool Shipped { get; set; }
 		[Required(ErrorMessage = "Введите имя")]
 		public string Name { get; set; }
-		[Required(ErrorMessage = "Введите первую строку адреса")]
+		[Required(ErrorMessage = "Введите улицу")]
 		public string Line1 { get; set; }
-		public string Line2 { get; set; }
-		public string Line3 { get; set; }
 		[Required(ErrorMessage = "Введите название города")]
 		public string City { get; set; }
-		[Required(ErrorMessage = "Введите название штата")]
-		public string State { get; set; }
 		public string Zip { get; set; }
-		[Required(ErrorMessage = "Введите название страны")]
-		public string Country { get; set; }
 		public string GiftWrap { get; set; }
 	}
 }
